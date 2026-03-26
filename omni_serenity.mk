48dffca0e9ffbe348e5bb45c6d8902cfc29dc958
Titus <156310498+LunaDevPH@users.noreply.github.com>
2026-03-26 13:00:29 +0800

    Update omni_serenity.mk

--- omni_serenity.mk
+++ omni_serenity.mk
@@ -1,9 +1,15 @@
+# Inherit from those product configuration files.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, vendor/twrp/artifacts/otacert.mk)
+
+# Inherit from TWRP (Updated for 12.1 Minimal)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 PRODUCT_DEVICE := serenity
 PRODUCT_NAME := omni_serenity
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi A5
 PRODUCT_MANUFACTURER := xiaomi
+
+# Fingerprint (Optional but helpful for MTP)
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
